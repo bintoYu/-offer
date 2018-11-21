9268a1c11ae5bd63c44e11f6eea4595122e5e979
bintoYu <bintoyu@126.com>
2018-11-21 23:43:34 +0800

    123

--- src/数据结构/用两个栈实现队列.java
+++ src/数据结构/用两个栈实现队列.java
@@ -2,7 +2,12 @@ package 数据结构;
 
 import java.util.Stack;
 
-
+/**
+	入队列：直接压入s1即可
+	出队列：如果s2不为空，把s2中的栈顶元素直接弹出；否则，把s1的所有元素全部弹出压入s2中，再弹出s2的栈顶元素
+ * @author binto
+ *
+ */
 public class 用两个栈实现队列
 {
     Stack<Integer> stack1 = new Stack<Integer>();
